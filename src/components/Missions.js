05a116cca732fadbdd595c064595846cf7eb4129
toyman640 <falakoadebayor@gmail.com>
2023-06-28 14:56:32 +0100

    Fetch api missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMissions } from '../redux/missions/missionsSlice';
 
-const Missions = () => (
-  <div>
-    <h1>Missions</h1>
-  </div>
-);
+const Missions = () => {
+  const dispatch = useDispatch();
+  const { missions } = useSelector((state) => state.missions);
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+  return (
+    <div>
+      <h1>Missions</h1>
+      <table>
+        <tr>
+          <th>Mission</th>
+          <th>Discription</th>
+          <th>Status</th>
+        </tr>
+        {missions.map((mission) => (
+          <tr key={mission.id}>
+            <td>{mission.name}</td>
+            <td>{mission.description}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+};
 
 export default Missions;
