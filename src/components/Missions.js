c1fd6f12ab85fc33bf90508e2878627dca9d582a
toyman640 <falakoadebayor@gmail.com>
2023-06-30 13:34:40 +0100

    Add joined missions to profile

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,44 +1,48 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchMissions, toggleMissionStatus } from '../redux/missions/missionsSlice';
+import { fetchMissions, joinMission, leaveMission } from '../redux/missions/missionsSlice';
+import '../App.css';
 
 const Missions = () => {
   const dispatch = useDispatch();
-  const { missions } = useSelector((state) => state.missions);
+  const missions = useSelector((state) => state.missions.missions);
   useEffect(() => {
     dispatch(fetchMissions());
   }, [dispatch]);
   const handleMission = (missionId, currentStatus) => {
-    const newStatus = !currentStatus;
-    dispatch(toggleMissionStatus({ missionId, newStatus }));
+    if (currentStatus) {
+      dispatch(leaveMission(missionId));
+    } else {
+      dispatch(joinMission(missionId));
+    }
   };
   return (
     <div>
-      <h1>Missions</h1>
-      <table>
+      <table className="Mission-table">
         <thead>
-          <th>Mission</th>
-          <th>Discription</th>
-          <th>Status</th>
+          <tr className="Table-head">
+            <th className="Mission-title">Mission</th>
+            <th className="Desc-title">Discription</th>
+            <th className="Desc-title">Status</th>
+            <th className="Desc-title-2"> </th>
+          </tr>
         </thead>
-        {missions.map((mission) => (
+        {missions.map((mission, index) => (
           <tbody key={mission.id}>
-            <td>{mission.name}</td>
-            <td>{mission.description}</td>
-            <td>
-              {mission.status ? (
-                <button type="button">Active Member</button>
-              ) : (
-                <button type="button">Not a member</button>
-              )}
-            </td>
-            <td>
-              {mission.status ? (
-                <button type="button" onClick={() => handleMission(mission.id, mission.status)}>leave mission</button>
-              ) : (
-                <button type="button" onClick={() => handleMission(mission.id, mission.status)}>Join Mission</button>
-              )}
-            </td>
+            <tr className={index % 2 === 1 ? 'Even-row' : 'Odd-row'}>
+              <td className="Mission-name">{mission.name}</td>
+              <td className="Description">{mission.description}</td>
+              <td className="Status">
+                <button type="button" className={mission.reserved ? 'Active-member' : 'Not-member'}>
+                  {mission.reserved ? 'Active Member' : 'Not a Member'}
+                </button>
+              </td>
+              <td className="Status-2">
+                <button type="button" onClick={() => handleMission(mission.id, mission.reserved)} className={mission.reserved ? 'Leave-mission' : 'Join-mission'}>
+                  {mission.reserved ? 'Leave Mission' : 'Join Mission'}
+                </button>
+              </td>
+            </tr>
           </tbody>
         ))}
       </table>
